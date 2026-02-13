80e047be3bc9196412737b0017fd0e9c1d9a5b5b
souzdc <souzadc.dev@gmail.com>
2026-02-13 17:54:34 -0300

    fix(playRaceEngine) bug wrong number of rounds fixed

--- Simulador Mario Kart/src/engine.ts
+++ Simulador Mario Kart/src/engine.ts
@@ -5,10 +5,37 @@ async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+async function getRandomBlock() {
+    let random = Math.random();
+    let result: string;
+
+    switch(true) {
+        case random < 0.33:
+            result = "RETA";
+            break;
+        
+        case random < 0.66:
+            result = "CURVA";
+            break;
+
+        default:
+            result = "CONFRONTO";
+    }
+
+    return result;
+}
+
+
+
+
 export async function playRaceEngine(char1: Player, char2: Player, rounds: number = 5) {
 
-    for(let rounds = 1; rounds <= 5; rounds++) {
-        console.log(`Rodada ${rounds}`);
+    for(let round = 1; round <= rounds; round++) {
+
+        let block = await getRandomBlock();
+        console.log(`Rodada ${round} Bloco: ${block}`);
+
+
     }
 
 }
